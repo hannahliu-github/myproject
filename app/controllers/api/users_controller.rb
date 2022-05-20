fa35ef9f15329d5ac650856601ff020a04c8d17c
hannahliu-github <shhan.liu@gmail.com>
2022-05-19 21:46:36 -0400

    Backend for User Authentication

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,29 @@
+class Api::UsersController < ApplicationController
+
+    before_action :require_current_user!, except: [:create]
+
+    def create
+        @user = User.new(user_params)
+
+        begin 
+            user_saved_successfully = @user.save
+        rescue ActiveRecord::RecordNotUnique
+            # add duplicate username msg to username errors list
+            user_saved_successfully = false
+            @user.errors.message[:username].append("already exists")
+        end
+
+        if @user.user_saved_successfully
+            login!(@user)
+            render :show
+        else
+            render json: {user, nil, errors: @user.errors.messages}
+        end
+    end
+
+    private
+    # param helper
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  namespace :api, defaults: {format: :json } do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
+
   get 'static_pages/root'
   root 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
